545d7dee68ba0d8524d63a5c2a9672bbfe0c5775
Shrihari <shriharipapa@gmail.com>
2020-09-04 22:21:51 +0530

    JDBC

---
+++ JavaCodings/src/JDBCSample.java
@@ -0,0 +1,27 @@
+import java.sql.* ;
+
+import static java.lang.Class.forName;
+
+
+public class JDBCSample {
+    public static void main(String[] args) throws Exception {
+
+         String url = "jdbc:mysql://localhost:3306/Registration";
+         String root = "root";
+         String password = "password";
+         String query = "select * from user;";
+
+         forName("com.mysql.jdbc.Driver"); // Setting up drivers
+         Connection con = DriverManager.getConnection(url , root , password) ; // Connection Establish
+         Statement st = con.createStatement() ; // Statements
+         ResultSet res = st.executeQuery(query); // Execution statments
+         res.next();
+         String userData = res.getString(2) +" : " +
+                 res.getString(1)  + " : " +
+                 res.getString(3); // ResultSet to store data.
+         System.out.println(userData);
+         st.close();
+         con.close();
+
+    }
+}
