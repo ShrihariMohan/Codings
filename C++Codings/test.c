d894f41b892fdcdc2e0e7afe1f2860b520a8e5ce
Shrihari <shriharipapa@gmail.com>
2020-09-07 21:43:29 +0530

    Basics

---
+++ C++Codings/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main() {
+	int main = 5 ;
+	printf("%d", main);
+}
+
---
+++ C/characterRules.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main() {
+    int  a = 10 ; // 4 byte
+    float f  = 10 ;  // 4 byte
+    char ch = '\0'; // 1 byte
+    
+    printf ("%d %f %c", a , f , ch); 
+
+    //CHARCTER RULES
+    //1 .  single quotes must 
+    //2 . only one length
+    // 3. except escape sequence
+}
