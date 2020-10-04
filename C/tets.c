1520551effe5b7b10aaff15df200896fa4346051
Shrihari <shriharipapa@gmail.com>
2020-10-04 12:52:38 +0530

    TCS - Digital Problems in python

---
+++ C/tets.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 0 ;
+    while ( + ( + i --) != 0) {
+        printf("%d ",i);
+        i -= i + 1 ;
+        printf("%d \n",i);
+    }
+    printf("%d ",i);
+    return 0;
+}
