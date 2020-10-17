c3b546848936df8736670a883d03c436f139bc85
Shrihari <shriharipapa@gmail.com>
2020-10-17 19:34:59 +0530

    Java

---
+++ JavaCodings/JavaFullCourse/LanguageFundas/src/CJ1_Identifiers.java
@@ -0,0 +1,85 @@
+public class CJ1_Identifiers {
+    // A Name in Java Program is called Identifiers ..
+    // in java return type is mandatory but in c default is integer
+    public static void main(String[] args) {
+        int x = 10 ;
+        System.out.println(x);
+        // There are 5 Identifiers here
+        // String - also a Identifier
+        // Allowed Characters are A-Z a-z 0-9 _$
+        // There Are no length limits - 1C also possibles
+
+        int String = 10 ;
+        int Runnable = 12 ;
+        System.out.println(String + Runnable);
+
+        // In Java 53 Reserved keywords are there
+        // 50 - keywords
+        //      2 unused keywords [ goto , const]
+        //      48 used [ if , else , int .. ]
+        // 3 - literals [ true , false , null ]
+
+        /*
+        Keywords for data types - 8
+        1. byte
+        2. short
+        3. int
+        4. float
+        5. long
+        6. double
+        7. boolean
+        8. char
+
+        Keywords for flow control - 11
+        9. switch
+        10. for
+        11. while
+        12. break
+        13. continue
+        14. do
+        15. case
+        16. default
+        17. if
+        18. else
+        19. return
+
+        Keywords for modifiers -11
+        20. public
+        21. private
+        22. protected
+        23. static
+        24. final
+        25. abstract
+        26. synchornized
+        27. native
+        28. volatile
+        29. strictfp
+        30. transient
+
+        Keywords for exception handling - 6
+        try
+        catch
+        finally
+        assert
+        throws
+        throw
+
+        keywords for class related - 6
+        class
+        interface
+        extends
+        implements
+        package
+        import
+
+       keywords for object related - 4
+       new
+       instanceof
+       super
+       this
+
+       void return type keyword - 1
+       enum - group of named constants
+       */
+    }
+}
--- PyCodings/test.py
+++ PyCodings/test.py
@@ -1,1 +1,8 @@
-print ( "Hello World")
+#!/usr/bin/python3
+def power(num,idx):
+    if(idx==1):
+       return(num)
+    else:
+       return(num * power(num,idx-1))
+
+print ( power(2 , 3))
