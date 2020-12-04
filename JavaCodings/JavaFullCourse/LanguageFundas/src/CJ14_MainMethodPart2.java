93fb09401aca1b1ab5519fdf3de4c6c7935f97c0
Shrihari <shriharipapa@gmail.com>
2020-12-04 19:01:54 +0530

    MainMethods

---
+++ JavaCodings/JavaFullCourse/LanguageFundas/src/CJ14_MainMethodPart2.java
@@ -0,0 +1,5 @@
+public class CJ14_MainMethodPart2 {
+    // 1.7v enhancements related to main method
+    // please declare main method as public static void main( String[] args)
+    // more meaningful error
+}
